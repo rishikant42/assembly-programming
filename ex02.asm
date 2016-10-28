459ff96eeb93371b280586e930f061d90daac21c
rishikant42 <rksbtp@gmail.com>
2016-10-28 21:27:03 +0530

    program to add three numbers using memory variables

--- ex02.asm
+++ ex02.asm
@@ -1,11 +1,11 @@
 [ORG 0x100]
-	mov	ax, [Num1]
-	mov 	bx, [Num2]
-	add	ax, bx
-	mov	bx, [Num3]
-	add	ax, bx
-	mov	[Num4], ax
-	mov	ax, 0x4C00
+	mov	ax, [Num1]      ; load first number in ax  
+	mov 	bx, [Num2]    ; load second number in bx
+	add	ax, bx          ; accumulate sum in ax
+	mov	bx, [Num3]      ; load third number in bx
+	add	ax, bx          ; accumulate sum in ax
+	mov	[Num4], ax      ; store sum in num4
+	mov	ax, 0x4C00      ; terminate program
 	int	0x21
 ;execution never comes at this line
 Num1:	dw	5
