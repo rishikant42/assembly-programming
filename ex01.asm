c8f6a410b7a42072e6da36acd6f9d3dd836fac39
rishikant42 <rksbtp@gmail.com>
2016-10-28 21:21:40 +0530

    a program to add three numbers using register

--- ex01.asm
+++ ex01.asm
@@ -1,10 +1,10 @@
 [ORG 0x100]
 
-mov ax, 5
-mov bx, 10
-add ax, bx
-mov bx, 15
-add ax, bx
+mov ax, 5            ; load first number in ax
+mov bx, 10           ; load second number in bx
+add ax, bx           ; accumulate sum in ax
+mov bx, 15           ; load third number in bx
+add ax, bx           ; accumulate sum in ax
 
-mov ax, 0x4c00
+mov ax, 0x4c00       ; terminate program
 int 0x21
