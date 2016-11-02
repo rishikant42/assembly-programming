e80c7edb92d916a106cc21ca41ff7963e87e57aa
rishikant42 <rksbtp@gmail.com>
2016-11-02 21:31:29 +0530

    program to add ten no without a seprate counter

---
+++ ex10.asm
@@ -0,0 +1,17 @@
+[org 0x0100]
+
+num1:	dw 10, 20, 30, 40, 50, 10, 20, 30, 40, 50
+total: dw 0
+
+start:	mov bx, 0
+	mov ax, 0
+
+l1:	add ax, [num1+bx]
+	add bx, 2
+	cmp bx, 20
+	jne l1
+
+	mov [total], ax
+
+	mov ax, 0x4c00
+	int 0x21
